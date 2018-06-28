ab5ece2732d40e1aabee3bd850ff8299b817dfb2
amresh03 <amresh.ramkhelavan@photoninfotech.net>
2018-06-28 15:43:47 +0530

    webhooks checking

--- src/main/java/com/example/ec2springbootdemo/Ec2SpringbootDemoApplication.java
+++ src/main/java/com/example/ec2springbootdemo/Ec2SpringbootDemoApplication.java
@@ -13,6 +13,7 @@ public class Ec2SpringbootDemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Ec2SpringbootDemoApplication started");
 		SpringApplication.run(Ec2SpringbootDemoApplication.class, args);
+		System.out.println("Ec2SpringbootDemoApplication ended");
 	}
 	@GetMapping("/api/msg/{name}")
 	public String getMessage(@PathVariable String name){
